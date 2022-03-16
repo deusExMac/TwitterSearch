5fc61ed6cfe0db63c1821542d69302b7f18f3ba5
deusExMac <39963356+deusExMac@users.noreply.github.com>
2022-03-16 22:29:03 +0200

    Moved function breaking up date range into utils.py

---
+++ utils.py
@@ -0,0 +1,83 @@
+import datetime
+import dateutil.parser
+from datetime import datetime, timedelta
+import configparser
+
+
+#
+# Generates periods based on steps specified by t
+# NOTE: from date f, until date u must be strings
+# formatted in ISO time (format %Y-%m-%dT%H:%M:%SZ).
+#
+def breakUpDate(f, u, t, cfg=None ):
+
+      
+      # Configuration is required
+      if cfg is None:
+         return(None)
+
+        
+      tPeriods = []
+    
+      if datetime.strptime(f, "%Y-%m-%dT%H:%M:%SZ") > datetime.strptime(u, "%Y-%m-%dT%H:%M:%SZ"):
+       print('Invalid dates: End-date [', u, '] must be after start-date [', f, ']') 
+       return(None)
+      
+      if t == "":
+       tPeriods.append( {'from': f, 'until': u} )
+       if cfg.getboolean('Debug', 'debugMode', fallback=False):  
+          print("\t[DEBUG] No time step specified. Adding SINGLE search period: [", datetime.strptime(f, "%Y-%m-%dT%H:%M:%SZ").strftime("%d/%m/%Y %H:%M:%S"), "-", datetime.strptime(u, "%Y-%m-%dT%H:%M:%SZ").strftime("%d/%m/%Y %H:%M:%S"), "]")
+
+       return(tPeriods)
+
+      # Do some normalization if needed
+      # TODO: DO we need this check????
+      if 'D' not in t:
+        return(None)
+    
+      if 'H' not in t:
+        t += '0H0M0S'
+      elif 'M' not in t:
+          t += '0M0S'
+      elif 'S' not in t:
+          t +='0S'
+
+      #parse time period step
+      dayVal = -1 # Days are handled in a special way because we use existing date parsing routines and number of days cannot be 0 or exceed 31    
+      if t.startswith('0D'):
+       t = t.replace('0D', '')
+       tmFormat = '%HH%MM%SS'
+       dayVal = 0
+      else:
+        tmFormat = '%dD%HH%MM%SS'
+
+      try: 
+        stepValue =  datetime.strptime(t, tmFormat)
+        if dayVal != 0:
+           dayVal = stepValue.day 
+      except Exception as sEx:
+        print( "Invalid time step ", str(sEx))
+        return(None)
+           
+      sDate = datetime.strptime(f, "%Y-%m-%dT%H:%M:%SZ")
+      pCnt = 0
+      while True:
+        eDate = sDate + timedelta(days= dayVal, hours = stepValue.hour, minutes = stepValue.minute, seconds=(stepValue.second) )                                                            
+        if eDate >= datetime.strptime(u, "%Y-%m-%dT%H:%M:%SZ"):
+           eDate = datetime.strptime(u, "%Y-%m-%dT%H:%M:%SZ")
+           tPeriods.append( {'from': sDate.strftime("%Y-%m-%dT%H:%M:%SZ"), 'until': eDate.strftime("%Y-%m-%dT%H:%M:%SZ")} )
+           pCnt += 1
+           if cfg.getboolean('Debug', 'debugMode', fallback=False):
+              print("\t[DEBUG] Adding search period: [", sDate.strftime("%d/%m/%Y %H:%M:%S"), "-", eDate.strftime("%d/%m/%Y %H:%M:%S"), "]")
+              
+           return(tPeriods)
+      
+        if cfg.getboolean('Debug', 'debugMode', fallback=False):
+           print("\t[DEBUG] Adding search period: [", sDate.strftime("%d/%m/%Y %H:%M:%S"), "-", eDate.strftime("%d/%m/%Y %H:%M:%S"), "]")
+           
+        tPeriods.append( {'from': sDate.strftime("%Y-%m-%dT%H:%M:%SZ"), 'until': eDate.strftime("%Y-%m-%dT%H:%M:%SZ")} )
+        pCnt += 1
+        sDate = sDate + timedelta(days= dayVal, hours = stepValue.hour, minutes = stepValue.minute, seconds=stepValue.second)
+
+      # Remove this?
+      return(tPeriods)
